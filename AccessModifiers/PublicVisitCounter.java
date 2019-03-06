e9b6f15b1bf2dec49f9b5dd9374b1f344d362c31
Dorota Miler <dorota.miler@outlook.com>
2019-03-06 20:58:09 +0100

    PublicVisitCounter.java modification

---
+++ AccessModifiers/PublicVisitCounter.java
@@ -0,0 +1,10 @@
+public class PublicVisitCounter{
+    /* each user by calling method would
+     increment counter of webpage visits */
+
+public int userCount = 0;
+
+public void incrementCounter(){
+    userCount=++;
+}
+}
